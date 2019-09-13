bb67efa2ec6f7f5ab99f0b97e3373ea1a29f8dfe
Ekaterina Frolova <ekaterina.frolova40@gmail.com>
2019-09-13 17:56:11 +0300

    translations was added

---
+++ test_flask_blog/config.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    CSRF_ENABLED = True
+    DEBUG = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or \
+        "postgresql+psycopg2://Katerina:1234@localhost/flask_blog"
+
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = [os.environ.get('ADMIN'), '']
+
+    POSTS_PER_PAGE = 20
+
+    LANGUAGES = ['en', 'ru', 'be', 'es']
